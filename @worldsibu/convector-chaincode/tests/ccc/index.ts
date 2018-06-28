39cb1d56b707df3c1a0c1f33a15c1e8a7176f447
Diego Barahona <diestrin@gmail.com>
2018-06-28 01:38:54 +0000

    feat: Rename logically (pull request #21)
    * add base readme
    
    * rename folder structure logically
    
    * update peer dependencies version
    
    * fix unit tests on invokable

--- @worldsibu/convector-chaincode/tests/ccc/index.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Invokable, Param } from '@worldsibu/convector-controller';
-
-@Controller('test')
-export class TestController {
-  @Invokable()
-  public async test() {
-    // EMPTY
-  }
-}
