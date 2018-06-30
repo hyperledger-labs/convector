2a2ae0caed8b3bd4fab21a1eb78a475977990b6e
Diego Barahona <diestrin@gmail.com>
2018-06-30 16:27:24 +0000

    fix: Patches after release (pull request #26)
    * remove console logs
    
    * update clean env script
    
    Approved-by: Diego Barahona <diestrin@gmail.com>

--- @worldsibu/convector-tool-dev-env/scripts/clean.sh
+++ @worldsibu/convector-tool-dev-env/scripts/clean.sh
@@ -1,4 +1,4 @@
-docker rm -f $(docker ps -a | awk '$2~/hyperledger/ {print $1}') $(docker ps -a | awk '{print $15}' | grep \"^dev-peer\") || true
-docker rmi -f $(docker images | grep \"^dev-peer\" | awk '{print $3}') || true
+docker rm -f $(docker ps -a | awk '$2~/hyperledger/ {print $1}') $(docker ps -a | awk '{ print $1,$2 }' | grep dev-peer | awk '{print $1 }') || true
+docker rmi -f $(docker images | grep dev-peer | awk '{print $3}') || true
 
 rm -rf $PWD/.hfc-key-store
