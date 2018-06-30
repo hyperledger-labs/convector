479fa97c904fb75f805f6c28058fa5c99d3a6d27
Diego Barahona <diestrin@gmail.com>
2018-06-30 01:42:35 -0600

    update ci package versions

--- @worldsibu/convector-tool-chaincode-manager/src/manager.ts
+++ @worldsibu/convector-tool-chaincode-manager/src/manager.ts
@@ -180,6 +180,8 @@ export class Manager {
       return { ...packages, [name]: `file:./packages/${name}` };
     }, Promise.resolve({} as KV));
 
+    console.log(extraPackages);
+
     pkg.scripts = { start: pkg.scripts.start };
     pkg.dependencies = {
       ...pkg.dependencies,
