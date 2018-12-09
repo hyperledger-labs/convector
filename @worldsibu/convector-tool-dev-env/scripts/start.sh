2fd580ea59f89e0516c067ec00e237b2228da1b4
Luca Tamburrano <ltamburrano@gmail.com>
2018-12-09 21:09:11 +0100

    Migrated to 1.3 of Hyperledger Fabric
    Signed-off-by: Luca Tamburrano <ltamburrano@gmail.com>

--- @worldsibu/convector-tool-dev-env/scripts/start.sh
+++ @worldsibu/convector-tool-dev-env/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT_DIR=$(dirname "${0}")/..
 
-COMPOSE_PROJECT_NAME=net FABRIC_VERSION=x86_64-1.1.0 THIRDPARTY_VERSION=x86_64-0.4.6 \
+COMPOSE_PROJECT_NAME=net FABRIC_VERSION=1.3.0 THIRDPARTY_VERSION=0.4.13 \
   docker-compose -f $ROOT_DIR/docker-compose.yml up -d
