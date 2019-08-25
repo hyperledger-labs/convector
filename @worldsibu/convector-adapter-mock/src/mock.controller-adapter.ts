b2a26953b9d2dba32e8b154f42942eb5e167ba02
Diego Barahona <diestrin@gmail.com>
2019-08-25 15:47:16 -0600

    feat: mock adapter generate users
    Signed-off-by: Diego Barahona <diestrin@gmail.com>

--- @worldsibu/convector-adapter-mock/src/mock.controller-adapter.ts
+++ @worldsibu/convector-adapter-mock/src/mock.controller-adapter.ts
@@ -1,6 +1,7 @@
 /** @module @worldsibu/convector-adapter-mock */
 
 import * as uuid from 'uuid/v1';
+import * as selfsigned from 'selfsigned';
 import { Chaincode, IConfig } from '@worldsibu/convector-core-chaincode';
 import { ChaincodeMockStub, Transform } from '@theledger/fabric-mock-stub';
 import { ControllerAdapter, ClientResponseError } from '@worldsibu/convector-core';
@@ -10,7 +11,19 @@ import { ControllerAdapter, ClientResponseError } from '@worldsibu/convector-cor
 import { Transform as _Transform } from '@theledger/fabric-chaincode-utils';
 _Transform.isObject = data => data !== null && typeof data === 'object';
 
+export interface CertificateProps {
+  commonName: string;
+  countryName?: string;
+  localityName?: string;
+  stateOrProvinceName?: string;
+  organizationName?: string;
+  organizationalUnitName?: string;
+  emailAddress?: string;
+}
+
 export class MockControllerAdapter implements ControllerAdapter {
+  public users = {};
+
   public stub = new ChaincodeMockStub('Participant', new Chaincode());
 
   public async init(config: IConfig[]) {
@@ -24,7 +37,7 @@ export class MockControllerAdapter implements ControllerAdapter {
   }
 
   public async invoke(controller: string, name: string, config: any = {}, ...args: any[]) {
-    this.stub['usercert'] = config.user ? config.user : this.stub['usercert'];
+    this.stub['usercert'] = config.user ? this.users[config.user] || config.user : this.stub['usercert'];
 
     const transientMap = Object.keys(config.transient || {}).reduce((map, k) => {
       const v = config.transient[k];
@@ -60,5 +73,22 @@ export class MockControllerAdapter implements ControllerAdapter {
       ...response,
       result: Transform.bufferToObject(response.payload),
     };
+  }
+
+  addUser(name: string);
+  addUser(props: CertificateProps);
+  addUser(props: string|CertificateProps) {
+    let name: string;
+
+    if (typeof props === 'string') {
+      name = props;
+      props = {commonName: props};
+    } else {
+      name = props.commonName;
+    }
+
+    const attrsFormatted = Object.keys(props).map(k => ({ name: k, value: props[k] }));
+    const pems = selfsigned.generate(attrsFormatted);
+    this.users[name] = pems.cert;
   }
 }
